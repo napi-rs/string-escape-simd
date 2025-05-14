d79048cefc115dbc138c8ee63b4c975a7392f9c8
LongYinan <lynweklm@gmail.com>
2025-05-14 18:04:43 +0800

    Prefetch cache line ahead in loop (#3)

--- examples/escape.rs
+++ examples/escape.rs
@@ -1,6 +1,8 @@
-use string_escape_simd::encode_str;
+use string_escape_simd::{encode_str, encode_str_fallback};
 
 fn main() {
     let fixture = include_str!("../cal.com.tsx");
-    encode_str(fixture);
+    let encoded = encode_str(fixture);
+    let encoded_fallback = encode_str_fallback(fixture);
+    assert_eq!(encoded, encoded_fallback);
 }
